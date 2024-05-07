9b6cc6b222db82180716e94263b67f2c84ea7295
andresort18 <130782961+andresort18@users.noreply.github.com>
2024-05-07 09:56:00 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
-document.querySelector('.tecla_pom');
+function playSonido (idElementoAudio){
+    document.querySelector(idElementoAudio).play(); // seleccionamos el sonido y el .play para reproducirlo
+ }
+                                                                    
+// document.querySelector('.tecla_pom').onclick = playSonidoPom; // Seleccionamos objeto por objeto
+
+
+const listaDeTeclas = document.querySelectorAll('.tecla'); // Esto nos selecciona todos los elementos con ese nombre de clase
+
+var contador = 0;
+
+while (contador < 9){
+    const instrumento = listaDeTeclas[contador].classList[1];
+    console.log(instrumento);
+    listaDeTeclas[contador].onclick = function(){
+      playSonido(instrumento);
+    }
+    contador = contador + 1;
+    console.log('vuelta' + contador);
+}
